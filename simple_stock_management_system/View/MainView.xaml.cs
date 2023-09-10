3f96a02a436ff5c1f1e8a79b287a119c36e039bb
itzluminara <recon.mikasaackerman@gmail.com>
2023-09-10 22:18:22 +0530

    commit

--- simple_stock_management_system/View/MainView.xaml.cs
+++ simple_stock_management_system/View/MainView.xaml.cs
@@ -42,7 +42,46 @@ namespace simple_stock_management_system.View {
             
             dashboardView.BeginAnimation(OpacityProperty, fadeInAnimation);
         }
+
+        private void btnLog_Click(object sender, RoutedEventArgs e) {
+            LogView logView = new LogView();
+            logView.Opacity = 0;
+            logView.Loaded += LogView_Loaded;
+            Visibility = Visibility.Hidden;
+            logView.Show();
+        }
         
+        private void LogView_Loaded(object sender, RoutedEventArgs e) {
+            LogView logView = (LogView)sender;
+
+            DoubleAnimation fadeInAnimation = new DoubleAnimation {
+                From = 0,
+                To = 1,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+            
+            logView.BeginAnimation(OpacityProperty, fadeInAnimation);
+        }
+
+        private void btnStock_Click(object sender, RoutedEventArgs e) {
+            StockView stockView = new StockView();
+            stockView.Opacity = 0;
+            stockView.Loaded += StockView_Loaded;
+            Visibility = Visibility.Hidden;
+            stockView.Show();
+        }
+
+        private void StockView_Loaded(object sender, RoutedEventArgs e) {
+            StockView stockView = (StockView)sender;
+            
+            DoubleAnimation fadeInAnimation = new DoubleAnimation {
+                From = 0,
+                To = 1,
+                Duration = new Duration(TimeSpan.FromSeconds(0.2))
+            };
+            
+            stockView.BeginAnimation(OpacityProperty, fadeInAnimation);
+        }
     }
 }
 
---
+++ simple_stock_management_system/View/StockView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace simple_stock_management_system.View; 
+
+public partial class StockView : Window {
+    public StockView() {
+        InitializeComponent();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+        if (e.LeftButton == MouseButtonState.Pressed) {
+            DragMove();
+        }
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e) {
+        WindowState = WindowState.Minimized;
+    }
+    
+    private void btnClose_Click(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
+    }
+
+    private void BtnHome_OnClick(object sender, RoutedEventArgs e) {
+        MainView mainView = new MainView();
+        mainView.Opacity = 0;
+        mainView.Loaded += MainView_Loaded;
+        Visibility = Visibility.Hidden;
+        mainView.Show();
+    }
+
+    private void MainView_Loaded(object sender, RoutedEventArgs e) {
+        MainView mainView = (MainView)sender;
+        
+        DoubleAnimation fadeInAnimation = new DoubleAnimation {
+            From = 0,
+            To = 1,
+            Duration = new Duration(TimeSpan.FromSeconds(0.2))
+        };
+        
+        mainView.BeginAnimation(OpacityProperty, fadeInAnimation);
+    }
+}
+
